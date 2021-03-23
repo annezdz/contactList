4971000625c15200d5d7d6a7a8ee995ddd6b52ab
annezdz <annezdz@hotmail.com>
2021-03-22 21:49:17 -0300

    Added drawer_menu.xml , menu.xml and changed activity_main.xml and MainActivity.kt

--- app/src/main/java/com/example/listacontatos/ContactAdapter.kt
+++ app/src/main/java/com/example/listacontatos/ContactAdapter.kt
@@ -1,7 +1,10 @@
 package com.example.listacontatos
 
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
 class ContactAdapter: RecyclerView.Adapter<ContactAdapter.ContactAdapterViewHolder>() {
@@ -9,7 +12,8 @@ class ContactAdapter: RecyclerView.Adapter<ContactAdapter.ContactAdapterViewHold
     private val list:MutableList<Contact> = mutableListOf()
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ContactAdapterViewHolder {
-        TODO("Not yet implemented")
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.contact_item,parent,false)
+        return ContactAdapterViewHolder(view)
     }
 
     override fun getItemCount(): Int {
@@ -20,9 +24,20 @@ class ContactAdapter: RecyclerView.Adapter<ContactAdapter.ContactAdapterViewHold
         holder.bind(list[position])
     }
 
-    class ContactAdapterViewHolder(itemView : View) : RecyclerView.ViewHolder(itemView){
-        fun bind(contact:Contact){
+    fun updateList(list : List<Contact>){
+        this.list.clear()
+        this.list.addAll(list)
+        notifyDataSetChanged()
+    }
 
+    class ContactAdapterViewHolder(itemView : View) : RecyclerView.ViewHolder(itemView){
+        private val tvName: TextView = itemView.findViewById(R.id.tv_name)
+        private val tvPhone: TextView = itemView.findViewById(R.id.tv_phone)
+        private val imgPhotograph: ImageView = itemView.findViewById(R.id.iv_photograph)
+
+        fun bind(contact:Contact){
+            tvName.text = contact.name
+            tvPhone.text = contact.phone
         }
 
     }
--- app/src/main/java/com/example/listacontatos/MainActivity.kt
+++ app/src/main/java/com/example/listacontatos/MainActivity.kt
@@ -2,10 +2,52 @@ package com.example.listacontatos
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.widget.Toolbar
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
+
+    private val rvList : RecyclerView by lazy{
+        findViewById<RecyclerView>(R.id.rv_list)
+    }
+    private val adapter = ContactAdapter()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.drawer_menu)
+        initDrawer()
+        bindViews()
+        updateList()
+    }
+
+    private fun initDrawer(){
+        val drawerLayout = findViewById<View>(R.id.drawer_layout) as DrawerLayout
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
+
+        val toogle = ActionBarDrawerToggle(this,drawerLayout,toolbar,R.string.open_drawer,R.string.close_drawer)
+        drawerLayout.addDrawerListener(toogle)
+        toogle.syncState()
+    }
+
+    private fun bindViews(){
+        rvList.adapter = adapter
+        rvList.layoutManager = LinearLayoutManager(this)
+    }
+
+    private fun updateList() {
+        adapter.updateList(
+            arrayListOf(
+                Contact(
+                    "Igor Ferrani",
+                    "(00)0000-0000",
+                    "img.png"
+
+                )
+            )
+        )
     }
 }
